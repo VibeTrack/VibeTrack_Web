9eefb9f7a5fe6c1523406f820c06be6bbe8b7c63
MT XA <21020382@vnu.edu.vn>
2024-11-17 20:53:28 +0700

    update: update homeview

--- vibetrack/src/views/HomeView.vue
+++ vibetrack/src/views/HomeView.vue
@@ -2,11 +2,12 @@
 import MixesInspiredBy from '@/components/MixesInspiredBy.vue';
 import MultiArtistSelect from '@/components/MultiArtistSelect.vue';
 import CustomCarousel from '@/components/CustomCarousel.vue';
+import Highlights from '@/components/Highlights.vue';
 </script>
 
 <template>
   <div class="max-w-[1500px] mx-auto">
-    <div class="px-8 mt-8 min-w-[800px] w-fill">
+    <div class="px-8 mt-8 min-w-[800px] w-full">
       <div class="text-white text-xl font-semibold inline-block">
         Mixes inspried by...
         <div class="text-sm font-light text-[#A2A2AD]">Discover new tracks similar to your favourites</div>
@@ -16,187 +17,189 @@ import CustomCarousel from '@/components/CustomCarousel.vue';
 
       <div class="flex justify-between w-full gap-8">
         <div class="xl:w-1/3 w-1/2">
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Đừng làm trái tim anh đau"
-            by="Sơn Tùng M-TP" 
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
+          <MixesInspiredBy class="pb-1.5" text="Đừng làm trái tim anh đau" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
 
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
 
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
+        </div>
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy class="pb-1.5" text="Đừng làm trái tim anh đau" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
+
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
+
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
           />
         </div>
         <div class="xl:w-1/3 w-1/2">
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Đừng làm trái tim anh đau"
-            by="Sơn Tùng M-TP" 
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
+          <MixesInspiredBy class="pb-1.5" text="Đừng làm trái tim anh đau" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
 
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
 
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
-          />
-        </div>
-        <div class="xl:w-1/3 w-1/2">
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Đừng làm trái tim anh đau"
-            by="Sơn Tùng M-TP" 
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
-
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
-
-          <MixesInspiredBy 
-            class="pb-1.5"
-            text="Chúng ta của tương lai"
-            by="Sơn Tùng M-TP"
-            to="/artist"
-            image="http://picsum.photos/id/147/300/300"
-          />
+          <MixesInspiredBy class="pb-1.5" text="Chúng ta của tương lai" by="Sơn Tùng M-TP" to="/artist"
+            image="http://picsum.photos/id/147/300/300" />
         </div>
       </div>
     </div>
 
     <div class="px-8 mt-8 min-w-[800px]">
       <div class="text-white text-xl font-semibold inline-block">
-        Made for you 
+        Made for you
       </div>
 
       <div class="py-3"></div>
 
       <div class="flex justify-start gap-7">
-        <MultiArtistSelect
-          class="w-1/4"
-          category="daily"
-          text="Playlist Sơn Tùng"
-          to="/artist"
-          :images="{
-            one: 'https://picsum.photos/id/45/300/300',
-            two: 'https://picsum.photos/id/55/300/300',
-            three: 'https://picsum.photos/id/54/300/300',
-            four: 'https://picsum.photos/id/76/300/300',
-          }"
-        />
-        <MultiArtistSelect
-          class="w-1/4"
-          category="daily"
-          text="Fan J97 "
-          to="/artist"
-          :images="{
-            one: 'https://picsum.photos/id/45/300/300',
-            two: 'https://picsum.photos/id/55/300/300',
-            three: 'https://picsum.photos/id/54/300/300',
-            four: 'https://picsum.photos/id/76/300/300',
-          }"
-        />
-        <MultiArtistSelect
-          class="w-1/4"
-          category="daily"
-          text="Vinh què"
-          to="/artist"
-          :images="{
-            one: 'https://picsum.photos/id/45/300/300',
-            two: 'https://picsum.photos/id/55/300/300',
-            three: 'https://picsum.photos/id/54/300/300',
-            four: 'https://picsum.photos/id/76/300/300',
-          }"
-        />
-        <MultiArtistSelect
-          class="w-1/4"
-          category="daily"
-          text="Playlist"
-          to="/artist"
-          :images="{
-            one: 'https://picsum.photos/id/45/300/300',
-            two: 'https://picsum.photos/id/55/300/300',
-            three: 'https://picsum.photos/id/54/300/300',
-            four: 'https://picsum.photos/id/76/300/300',
-          }"
-        />
+        <MultiArtistSelect class="w-1/4" category="daily" text="Playlist Sơn Tùng" to="/artist" :images="{
+          one: 'https://picsum.photos/id/45/300/300',
+          two: 'https://picsum.photos/id/55/300/300',
+          three: 'https://picsum.photos/id/54/300/300',
+          four: 'https://picsum.photos/id/76/300/300',
+        }" />
+        <MultiArtistSelect class="w-1/4" category="daily" text="Fan J97 " to="/artist" :images="{
+          one: 'https://picsum.photos/id/45/300/300',
+          two: 'https://picsum.photos/id/55/300/300',
+          three: 'https://picsum.photos/id/54/300/300',
+          four: 'https://picsum.photos/id/76/300/300',
+        }" />
+        <MultiArtistSelect class="w-1/4" category="daily" text="Vinh què" to="/artist" :images="{
+          one: 'https://picsum.photos/id/45/300/300',
+          two: 'https://picsum.photos/id/55/300/300',
+          three: 'https://picsum.photos/id/54/300/300',
+          four: 'https://picsum.photos/id/76/300/300',
+        }" />
+        <MultiArtistSelect class="w-1/4" category="daily" text="Playlist" to="/artist" :images="{
+          one: 'https://picsum.photos/id/45/300/300',
+          two: 'https://picsum.photos/id/55/300/300',
+          three: 'https://picsum.photos/id/54/300/300',
+          four: 'https://picsum.photos/id/76/300/300',
+        }" />
       </div>
     </div>
 
     <div class="px-8 mt-8 min-w-[800px]">
       <div class="py-3"></div>
 
-      <p class="text-center pb-6 text-[#D1D1D6] text-sm">Unlock Flow by selecting your favourite artists for improved</p>
+      <p class="text-center pb-6 text-[#D1D1D6] text-sm">Unlock Flow by selecting your favourite artists for improved
+      </p>
 
       <div class="flex justify-center gap-7 w-full">
         <div class="flex items-center justify-center relative">
-          <img width="100" class="absolute -left-36 rounded-full z-0 cursor-pointer" src="https://picsum.photos/id/76/300/300">
-          <img width="115" class="absolute -left-[80px] rounded-full z-10 cursor-pointer" src="https://picsum.photos/id/76/300/300">
+          <img width="100" class="absolute -left-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/76/300/300">
+          <img width="115" class="absolute -left-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/76/300/300">
           <img width="160" class="rounded-full z-0 cursor-pointer" src="https://picsum.photos/id/76/300/300">
-          <img width="115" class="absolute -right-[80px] rounded-full z-10 cursor-pointer" src="https://picsum.photos/id/76/300/300">
-          <img width="100" class="absolute -right-36 rounded-full z-0 cursor-pointer" src="https://picsum.photos/id/76/300/300">
+          <img width="115" class="absolute -right-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/76/300/300">
+          <img width="100" class="absolute -right-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/76/300/300">
         </div>
       </div>
 
       <div class="w-full flex justify-center p-3">
-        <button
-          type="button"
-          class="text-white text-[15px] font-semibold rounded-full bg-[#EF5465] hover:bg-[#d8384a] px-8 py-2 text-center"
-        >
+        <button type="button"
+          class="text-white text-[15px] font-semibold rounded-full bg-[#EF5465] hover:bg-[#d8384a] px-8 py-2 text-center">
           ADD ARTIST
         </button>
       </div>
     </div>
 
     <div class="mt-8 min-w-[800px]">
-      <CustomCarousel 
-        category="New releases for you"
-        :data = "[
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-          { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: ''},
-        ]"
-      />
+      <CustomCarousel category="New releases for you" :data="[
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+      ]" />
     </div>
+
+    <div class="px-8 mt-8 min-w-[800px]">
+      <div class="text-white text-xl font-semibold inline-block">
+        Highlights
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex items-center gap-8">
+        <div class="rounded-lg w-1/2">
+          <Highlights by="Some Artist" song="Some music" image="https://picsum.photos/id/177/800/300" />
+        </div>
+        <div class="rounded-lg w-1/2">
+          <Highlights by="Some Artist 2" song="Somebody" image="https://picsum.photos/id/229/800/300" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-8 min-w-[800px]">
+      <CustomCarousel category="New releases for you" :data="[
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+        { url: 'https://picsum.photos/id/45/300/300', song: 'Something', by: '', releasedOn: '' },
+      ]" />
+    </div>
+
+    <div class="px-8 mt-8 min-w-[800px]">
+      <div class="text-white text-xl font-semibold inline-block">
+        Highlights
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex items-center gap-8">
+        <div class="rounded-lg w-1/2">
+          <Highlights by="Some Artist 1" song="Awsome Song for you!" image="https://picsum.photos/id/177/800/300" />
+        </div>
+        <div class="rounded-lg w-1/2">
+          <Highlights by="Some Artist 2" song="Another banger BRUV!" image="https://picsum.photos/id/126/800/300" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-8 min-w-[800px]">
+      <CustomCarousel category="New releases for you" :data="[
+        { url: 'https://picsum.photos/id/134/300/300', song: 'Foregone', by: 'In Flames', releasedOn: '10/02/2023' },
+        { url: 'https://picsum.photos/id/144/300/300', song: 'Dark Waters', by: 'Delain', releasedOn: '09/01/2023' },
+        { url: 'https://picsum.photos/id/146/300/300', song: 'Heritage', by: 'Distant', releasedOn: '10/01/2023' },
+        { url: 'https://picsum.photos/id/78/300/300', song: 'Meanwhile', by: 'Klone', releasedOn: '08/02/2023' },
+        { url: 'https://picsum.photos/id/151/300/300', song: 'Truth Decay', by: 'You Me At Six', releasedOn: '10/01/2023' },
+        { url: 'https://picsum.photos/id/155/300/300', song: 'This Is Why', by: 'Paramore', releasedOn: '10/02/2023' },
+        { url: 'https://picsum.photos/id/167/300/300', song: 'The Jaws Of Life', by: 'Pierce The Veil', releasedOn: '09/23/2023' },
+        { url: 'https://picsum.photos/id/191/300/300', song: 'The Future Is Your Past', by: 'In The Brian Jonestown Massacre', releasedOn: '07/25/2023' },
+        { url: 'https://picsum.photos/id/194/300/300', song: 'The Land, The Water, The Sky', by: 'Black Belt Eagle Scout', releasedOn: '09/22/2023' },
+        { url: 'https://picsum.photos/id/177/300/300', song: 'The Day My Father Died', by: 'SYML', releasedOn: '10/02/2023' },
+        { url: 'https://picsum.photos/id/227/300/300', song: 'Complete Mountain Almanac', by: 'Complete Mountain Almanac', releasedOn: '10/02/2023' },
+        { url: 'https://picsum.photos/id/232/300/300', song: 'Music From Montana Story', by: 'Kevin Morby', releasedOn: '10/01/2023' },
+      ]" />
+    </div>
+
+    <div class="pb-40"></div>
   </div>
 </template>
